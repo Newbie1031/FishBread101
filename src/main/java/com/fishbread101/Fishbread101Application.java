4ea956379730c072d6aa6e04e8736d8b4f66818f
Gwanho KIM <bigquan977@gmail.com>
2023-01-16 15:41:00 +0900

    [init] initproject

---
+++ src/main/java/com/fishbread101/Fishbread101Application.java
@@ -0,0 +1,13 @@
+package com.fishbread101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fishbread101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fishbread101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fishbread101/Fishbread101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fishbread101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fishbread101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
