960a5dada75dfad7fe8f549a160981ff6d64d40c
C one <oocone96@gmail.com>
2023-01-17 19:58:28 +0900

    Feat: Add AuthServiceImpl, SigininRequestDto, SignUpRequestDto

--- src/main/java/com/fishbread101/dto/SignUpRequestDto.java
+++ src/main/java/com/fishbread101/dto/SignUpRequestDto.java
@@ -1,4 +1,14 @@
 package com.fishbread101.dto;
 
+import com.fishbread101.entity.UserRole;
+import lombok.Getter;
+
+@Getter
 public class SignUpRequestDto {
+    private String loginId;
+    private String loginPw;
+    private Enum UserRole;
+    private boolean appliedTutor;
+    private String adminToken = "";
+    private String tutorToken = "";
 }
--- src/main/java/com/fishbread101/service/AuthServiceImpl.java
+++ src/main/java/com/fishbread101/service/AuthServiceImpl.java
@@ -1,4 +1,40 @@
 package com.fishbread101.service;
 
+import com.fishbread101.common.jwt.JwtUtil;
+
+import com.fishbread101.dto.SignInRequestDto;
+import com.fishbread101.dto.SignUpRequestDto;
+import com.fishbread101.dto.UserResponseDto;
+import com.fishbread101.entity.User;
+import com.fishbread101.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public UserResponseDto signUp(SignUpRequestDto signupRequestDto) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto signIn(SignInRequestDto signInRequestDto, HttpServletResponse response) {
+        return null;
+    }
+
+    @Override
+    public void signOut(User user) {
+
+    }
+//    private static final String ADMIN_TOKEN = // 토큰값 어디서 찾는지
+
+
 }
